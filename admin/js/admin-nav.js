ee1ce3e966b581a3bfb7d3e06dbb4f8a388a7d7c
Nguyễn Minh Việt Hoàng <viethoang26122004@gmail.com>
2025-12-30 15:02:37 +0700

    Update

--- admin/js/admin-nav.js
+++ admin/js/admin-nav.js
@@ -34,6 +34,12 @@ const adminConfig = {
                     icon: 'file-text', 
                     label: 'Tài liệu', 
                     href: 'documents.html'
+                },
+                { 
+                    id: 'posts', 
+                    icon: 'edit', 
+                    label: 'Bài viết', 
+                    href: 'posts.html'
                 }
             ]
         },
@@ -64,6 +70,7 @@ const icons = {
     'users': '<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>',
     'user': '<path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>',
     'file-text': '<path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>',
+    'edit': '<path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>',
     'headphones': '<path stroke-linecap="round" stroke-linejoin="round" d="M3 18v-6a9 9 0 0118 0v6M3 18a3 3 0 003 3h0a3 3 0 003-3v-3a3 3 0 00-3-3h0a3 3 0 00-3 3v3zm18 0a3 3 0 01-3 3h0a3 3 0 01-3-3v-3a3 3 0 013-3h0a3 3 0 013 3v3z"/>',
     'settings': '<path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>'
 };
@@ -203,6 +210,7 @@ if (document.readyState === 'loading') {
         if (path.includes('customers')) currentPage = 'customers';
         else if (path.includes('users')) currentPage = 'users';
         else if (path.includes('documents')) currentPage = 'documents';
+        else if (path.includes('posts')) currentPage = 'posts';
         else if (path.includes('tickets')) currentPage = 'tickets';
         else if (path.includes('settings')) currentPage = 'settings';
         
@@ -214,9 +222,12 @@ if (document.readyState === 'loading') {
     if (path.includes('customers')) currentPage = 'customers';
     else if (path.includes('users')) currentPage = 'users';
     else if (path.includes('documents')) currentPage = 'documents';
+    else if (path.includes('posts')) currentPage = 'posts';
     else if (path.includes('tickets')) currentPage = 'tickets';
     else if (path.includes('settings')) currentPage = 'settings';
     
     initAdminNav(currentPage);
 }
+
+
 
